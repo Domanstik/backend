c97df2cced4cdc5279e9374a1b19add6885ce794
DaniilMarinovich <marinovich.daniil@gmail.com>
2026-03-25 23:56:33 +0300

    API 14

--- StarMarathon.API/Program.cs
+++ StarMarathon.API/Program.cs
@@ -38,7 +38,7 @@ builder.Services.AddSingleton(supabaseClient);
 builder.Services.AddScoped<IFileStorageService, SupabaseStorageService>();
 builder.Services.AddHttpClient("StormAPI", client =>
 {
-    client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("STORM_API_URL") ?? " https://somewhat-fiscal-convenient-grey.trycloudflare.com/api/");
+    client.BaseAddress = new Uri(Environment.GetEnvironmentVariable("STORM_API_URL") ?? "https://wallpapers-signal-kai-certain.trycloudflare.com/api/");
 });
 builder.Services.AddScoped<IAuthService, StormBvService>();
 
